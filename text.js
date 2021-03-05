91c59d3e93d53e46aee9cf057056b1eca2c84893
MrAlone721 <80061696+MrAlone721@users.noreply.github.com>
2021-03-05 11:17:17 -0300

    Add files via upload
    Arquivos do aluno: Arthur Francisco Guedes Azevedo

---
+++ text.js
@@ -0,0 +1,7 @@
+	<script type="text/javascript">
+      function changeColor(){
+        document.querySelector("button").style.backgroundColor = "blue"; //change background color
+        document.querySelector("button").style.color = "white"; //change font color
+        document.querySelector("p").innerHTML = "Great! The button changed its color." //add text
+      }
+    </script>
